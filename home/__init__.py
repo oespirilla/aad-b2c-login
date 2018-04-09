d40251c7ef4e5fa5c4fe71967c17141e9347d31f
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-04-09 16:25:42 -0600

    added support for aad login

--- home/__init__.py
+++ home/__init__.py
@@ -3,7 +3,10 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_oauthlib.client import OAuth
 app = Flask(__name__)
 app.debug = True
+app.secret_key = '4b34b1c1d1a2f41ea9564c483311fbd9d07bc704b164ea2d'
+oauth = OAuth(app)
 
 import home.main
--- home/main.py
+++ home/main.py
@@ -4,8 +4,24 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
-from home import app
+from flask import render_template, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+
+from home import app, oauth
+
+
+microsoft = oauth.remote_app(
+	'microsoft',
+	consumer_key='8600e3dd-ca50-4abd-b1aa-d53ddfdb2cfb',
+	consumer_secret='xxxxxxxxxxx',
+	request_token_params={'scope': 'offline_access User.Read'},
+	base_url='https://graph.microsoft.com/v1.0/',
+	request_token_url=None,
+	access_token_method='POST',
+	access_token_url='https://login.microsoftonline.com/common/oauth2/v2.0/token',
+	authorize_url='https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+)
+
 
 @app.route('/')
 @app.route('/home')
@@ -16,3 +32,32 @@ def home():
         title='Home',
         year=datetime.now().year
     )
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+	return microsoft.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.pop('aad_token', None)
+    return redirect(url_for('home'))
+
+@app.route('/login/authorized')
+def authorized():
+
+    response = microsoft.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return "Access Denied: Reason=%s\nError=%s" % (response.get('error'),request.get('error_description'))
+
+    session['aad_token'] = (response['access_token'], '')
+
+    return redirect(url_for('home'))
+
+@microsoft.tokengetter
+def get_oauth_token():
+    return session.get('aad_token')
+
+@app.route('/me')
+def me():
+	me = microsoft.get('me')
+	return str(me.data)
