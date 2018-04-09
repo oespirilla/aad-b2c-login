11f185d6c2ea1c83e770277f2b005407a9aadf93
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-04-09 13:30:18 -0600

    first commit

---
+++ home/main.py
@@ -0,0 +1,18 @@
+
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from home import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home',
+        year=datetime.now().year
+    )
